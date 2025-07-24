59eddb657dd2f0a6eab6653be85aa9e5c9ad18d9
jp-work-free <jp-work-free@yandex.ru>
2025-07-24 21:42:43 +0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,22 +1,17 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-   $name = $_POST['name'];
-   $phone = $_POST['phone'];
-    if (!ctype_digit($phone)) {
-     echo "Номер телефона должен содержать только цифры.";
-     exit;
-       exit;
-   }
 
-   $to = 'jeka.wild.work@gmail.com';
-   $subject = 'Новый заказ обратного звонка';
-   $message = "Имя: $name\nТелефон: $phone";
-   if (mail($to, $subject, $message)) {
-           echo "Спасибо, мы вам перезвоним!";
-        exit; 
-    } else {
-       echo "Что-то пошло не так. Попробуйте ещё раз.";
-   }
-}
-?>
+$recepient = "jeka.wild.work@gmail.com";                    //Почта куда будут приходить письма
+$subject = "Заявка с сайта Форма обратной связи ajax";      //Сообщение
+$headers= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n"; 
+$headers .= "From: jp-work@yandex.ru <jp-work@yandex.ru>\r\n"; 
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["tel"]);
+$email = trim($_POST["email"]);
+// $massage = trim($_POST["massage"]);                      // Опционально, можно включить обратно
+
+$message = "Имя: $name <br/> Телефон: $phone <br/> Почта: $email";        // Опционально можно добавить после последнего <br/> - Сообщение: $massage"
+
+mail($recepient, $subject, $message, $headers);
 
